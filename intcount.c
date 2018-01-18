d5cd79019a9cc05750f4a21766dc684300529ade
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-01-18 16:46:39 +0530

    Update intcount.c

--- intcount.c
+++ intcount.c
@@ -3,15 +3,12 @@ int main()
 {
     long long n;
     int count = 0;
-
-    printf("Enter an integer: ");
+    printf("Enter an integer:");
     scanf("%lld", &n);
-
     while(n != 0)
     {
-        n /= 10;
-        ++count;
+       n /= 10;
+       ++count;
     }
-
-    printf("Number of digits: %d", count);
+   printf("Number of digits: %d",count);
 }
