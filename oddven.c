667b96f1c6fd5042bc6bdf6a00b5c0621f37e730
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-01-18 16:54:07 +0530

    Update oddven.c

--- oddven.c
+++ oddven.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include<conio.h>
 int main()
 {
-    int number;
+    int num;
     printf("Enter an integer: ");
-    scanf("%d", &number);
+    scanf("%d", &num);
     if(number % 2 == 0)
-        printf("%d is even.", number);
+        printf("%d is even.",num);
     else
-        printf("%d is odd.", number);
+        printf("%d is odd.",num);
     return 0;
+    getch();
 }
