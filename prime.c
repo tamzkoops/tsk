73f405400745daf7ae992e06a348a28c9ee93437
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-01-19 16:46:16 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, flag = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(i=2; i<=n/2; ++i)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (flag==0)
+        printf("%d yes.",n);
+    else
+        printf("%d No.",n);
+    return 0;
+}
