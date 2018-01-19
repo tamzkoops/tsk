00507476891290b2a54c2ea838ab2dc7a171abc8
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-01-19 16:00:00 +0530

    Create ntimes.c

---
+++ ntimes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main()
+{
+     int c, n;
+     printf("How many times you want to display hello world?\n");
+     scanf("%d", &n);
+     for (c=1; c<=n; c++)
+     printf("Hello world!\n");
+     return 0;
+}
