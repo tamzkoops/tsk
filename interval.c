c8dc6c3af058fb61be15671f6428c261f7a3b24f
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-01-23 15:58:46 +0530

    Update interval.c

--- interval.c
+++ interval.c
@@ -4,7 +4,7 @@ int main()
     int low, high, i, flag;
     printf("Enter two numbers(intervals):");
     scanf("%d %d", &low, &high);
-    printf("Prime numbers between %d and %d are: ", low, high);
+    printf("odd numbers between %d and %d are: ", low, high);
     while (low < high)
     {
         flag = 0;
