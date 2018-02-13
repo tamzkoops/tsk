4a2fe331b55701998e577dae05387e87bf05c972
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-02-13 08:43:28 +0000

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int K,n;
+    char S[10];
+    scanf("%s",S);
+    printf("Enter n:");
+    scanf("%d",&n);
+    for ( K = 1 ; K <= n ;K ++ )
+    {
+         printf ( "%s\n",S ) ;
+    }
+    return 0;
+}
