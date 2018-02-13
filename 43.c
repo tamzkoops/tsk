f0fc0463238e87081ba9fbe782a9e1138b5cf131
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-02-13 09:19:52 +0000

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char str1[20],str2[20];
+    printf("Enter 1st string:");
+    gets(str1);
+    printf("Enter 2nd string:");
+    gets(str2);
+    printf("%s%s",str1,str2);
+}
