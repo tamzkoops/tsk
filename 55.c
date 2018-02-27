6a8436c4b32bac45dbcbbe27610d0f47ec3af843
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-02-27 16:33:05 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+        int a,b,c;
+        scanf ("%d %d",&a,&b);
+        c=a+b;
+        if(c%2==0)
+        printf("Even");
+        else
+        printf("Odd");
+}
