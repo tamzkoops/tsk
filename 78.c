9fc4b844e500eef7f37a53be4e22c51027302c92
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-03-02 16:38:04 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int num;
+  char str[10];
+  printf("Enter the num:");
+  scanf("%d",&num);
+    if(num>0)
+  if(num%13==0)
+  printf("Yes");
+  else
+  printf("No");
+	return 0;
+
+}
