4d9e0a96f19ceac99e1d8e1ba2a59acfc1f1abd1
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-03-08 15:57:26 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int a,b,c,i,j;
+    printf("Enter the num a & b:");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf("diff: %d",a-b);
+    else
+    printf("diff:%d",b-a);
+    
+    
+
+    return 0;
+}
