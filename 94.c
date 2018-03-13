60407962d672786f9c1a36df0a4f9aa2774541a5
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-03-13 16:31:59 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) 
+{
+int a[10],n,i,sum=0,k;
+scanf("%d",&n);
+scanf("%d",&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+k=a[k];
+printf("%d",k);
+return 0;
+}
