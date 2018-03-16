dec7eaad028e58c1ebc8bbd9b54ca95fa26e0bb8
tamzkoops <30828365+tamzkoops@users.noreply.github.com>
2018-03-16 15:51:53 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str[100];
+int n,i,len;
+printf("\n Enter the string");
+scanf("%s",str);
+printf("\n N=");
+scanf("%d",&n);
+len=strlen(str);
+for(i=len-n;str[i]!='\0';i++)
+{
+printf("%c",str[i]);
+}
+}
